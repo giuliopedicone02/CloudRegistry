5770090f88c215bb8ed22b5a39d2761013ac96be
giuliopedicone02 <pediconegiulio02@gmail.com>
2026-02-02 15:21:30 +0100

    Fix Terraform syntax error in DynamoDB

--- main.tf
+++ main.tf
@@ -2,32 +2,49 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# 1. DYNAMODB
+# 1. DYNAMODB (CORRETTO: Niente punti e virgola)
 resource "aws_dynamodb_table" "db" {
   name         = "CloudRegistryDB"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "PK"
   range_key    = "SK"
-  attribute { name = "PK"; type = "S" }
-  attribute { name = "SK"; type = "S" }
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
 }
 
 # 2. COGNITO
 resource "aws_cognito_user_pool" "pool" {
   name = "RegistryPool"
-  admin_create_user_config { allow_admin_create_user_only = false }
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
   
   schema {
     attribute_data_type = "String"
     name                = "role"
     mutable             = true
-    string_attribute_constraints { min_length = 1; max_length = 20; }
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 20
+    }
   }
+
   schema {
     attribute_data_type = "String"
     name                = "classe"
     mutable             = true
-    string_attribute_constraints { min_length = 1; max_length = 10; }
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 10
+    }
   }
 }
 
@@ -44,15 +61,23 @@ resource "aws_sns_topic" "topic" {
 # 4. IAM ROLE (V5)
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda_registry_v5" 
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "lambda.amazonaws.com" } }]
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
   })
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy_v5"
   role = aws_iam_role.iam_for_lambda.id
+  
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -71,7 +96,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Effect   = "Allow"
         Resource = "*"
       },
-      # NUOVO PERMESSO FONDAMENTALE: Leggere gli utenti
+      # Permesso per leggere la lista studenti
       {
         Action   = ["cognito-idp:ListUsers"]
         Effect   = "Allow"
@@ -94,7 +119,6 @@ resource "aws_lambda_function" "backend" {
     variables = {
       TABLE_NAME    = aws_dynamodb_table.db.name
       SNS_TOPIC_ARN = aws_sns_topic.topic.arn
-      # NUOVA VARIABILE
       USER_POOL_ID  = aws_cognito_user_pool.pool.id
     }
   }
@@ -104,6 +128,7 @@ resource "aws_lambda_function" "backend" {
 resource "aws_apigatewayv2_api" "api" {
   name          = "RegistryAPI_v3"
   protocol_type = "HTTP"
+
   cors_configuration {
     allow_origins = ["*"]
     allow_methods = ["POST", "GET", "OPTIONS"]
@@ -113,8 +138,8 @@ resource "aws_apigatewayv2_api" "api" {
 }
 
 resource "aws_apigatewayv2_stage" "stage" {
-  api_id = aws_apigatewayv2_api.api.id
-  name   = "$default"
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
   auto_deploy = true
 }
 
@@ -125,4 +150,34 @@ resource "aws_apigatewayv2_integration" "integ" {
 }
 
 resource "aws_apigatewayv2_route" "route" {
-  api_id    = aws_apigateway
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /voto"
+  target    = "integrations/${aws_apigatewayv2_integration.integ.id}"
+}
+
+resource "aws_apigatewayv2_route" "route_get" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /voto"
+  target    = "integrations/${aws_apigatewayv2_integration.integ.id}"
+}
+
+resource "aws_lambda_permission" "api_perm" {
+  statement_id  = "AllowAPI_v5"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.backend.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+# OUTPUTS
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.pool.id
+}
+
+output "cognito_client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
+
+output "url_da_copiare" {
+  value = aws_apigatewayv2_stage.stage.invoke_url
+}
